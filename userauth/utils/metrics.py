411b7549e5ff9a57d7debbaf9c474d3ec5d53737
Eliseo Martelli <me@eliseomartelli.it>
2024-11-19 16:30:07 +0100

    feat: add more metrics in return

--- userauth/utils/metrics.py
+++ userauth/utils/metrics.py
@@ -10,23 +10,27 @@ def custom_roc_auc(y_true, y_scores):
     - y_scores: Predicted probabilities for the positive class.
 
     Returns:
-    - FALSE_POSITIVES: False Positive Rates
-    - TRUE_POSITIVE_RATE: True Positive Rates
-    - AREA_UNDER_CURVE: Area Under the ROC curve.
+    - fpr: false positive rates
+    - tpr: true positive rates
+    - auc: area under the roc curve.
+    - tp: true positives
+    - fp: false positives
+    - p: positives
+    - n: negatives
     """
     sorted_indices = np.argsort(y_scores)[::-1]
     y_true_sorted = y_true[sorted_indices]
 
-    TRUE_POSITIVES = np.cumsum(y_true_sorted)
-    FALSE_POSITIVES = np.cumsum(1 - y_true_sorted)
-    POSITIVES = np.sum(y_true)
-    NEGATIVES = len(y_true) - POSITIVES
+    tp = np.cumsum(y_true_sorted)
+    fp = np.cumsum(1 - y_true_sorted)
+    p = np.sum(y_true)
+    n = len(y_true) - p
 
-    TRUE_POSITIVE_RATE = TRUE_POSITIVES / POSITIVES
-    FALSE_POSITIVE_RATE = FALSE_POSITIVES / NEGATIVES
+    tpr = tp / p
+    fpr = fp / n
 
-    TRUE_POSITIVE_RATE = np.concatenate(([0], TRUE_POSITIVE_RATE))
-    FALSE_POSITIVE_RATE = np.concatenate(([0], FALSE_POSITIVE_RATE))
+    tpr = np.concatenate(([0], tpr))
+    fpr = np.concatenate(([0], fpr))
 
-    AREA_UNDER_CURVE = np.trapezoid(TRUE_POSITIVE_RATE, FALSE_POSITIVE_RATE)
-    return FALSE_POSITIVE_RATE, TRUE_POSITIVE_RATE, AREA_UNDER_CURVE
+    auc = np.trapezoid(tpr, fpr)
+    return fpr, tpr, auc, tp, fp, p, n
